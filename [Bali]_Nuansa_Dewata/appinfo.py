939b2ddede139602c3133e71ec0deb77ac9edbfe
ichadhr <ichadhr@gmail.com>
2018-10-04 14:35:01 +0700

    upload Nuansa Dewata

---
+++ [Bali]_Nuansa_Dewata/appinfo.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: ichadhr
+# @Date:   2018-07-16 10:10:23
+# @Last Modified by:   richard.hari@live.com
+# @Last Modified time: 2018-10-03 16:29:04
+
+_appname = "CSV Converter [Nuansa Dewata]"
+_version = "1.0.0.0"
+_by      = "richard.hari@live.com"
